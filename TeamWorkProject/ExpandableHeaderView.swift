c01066383d028b770dddd7d5ef776daa5e3948fc
miskeen110 <alijatoi110@gmail.com>
2017-08-20 18:44:06 +0300

    dummyheaderadded

--- TeamWorkProject/ExpandableHeaderView.swift
+++ TeamWorkProject/ExpandableHeaderView.swift
@@ -8,41 +8,46 @@
 
 import UIKit
 
-protocol ExpandableHeaderViewDelegage {
+protocol ExpandableHeaderViewDelegate {
     func toggleSection(header: ExpandableHeaderView, section: Int)
 }
 
 class ExpandableHeaderView: UITableViewHeaderFooterView {
-    var delegate: ExpandableHeaderViewDelegage?
+    var delegate: ExpandableHeaderViewDelegate?
     var section: Int!
     
-    override init(reuseIdentifier: String?){
+    override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(selectHeaderAction)))
-
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func selectHeaderAction(gestureRecognizer: UITapGestureRecognizer){
+    func selectHeaderAction(gestureRecognizer: UITapGestureRecognizer) {
         let cell = gestureRecognizer.view as! ExpandableHeaderView
         delegate?.toggleSection(header: self, section: cell.section)
-
     }
     
-    func customInit(title: String, section: Int, delegate: ExpandableHeaderViewDelegage){
+    func customInit(title: String, section: Int, delegate: ExpandableHeaderViewDelegate) {
         self.textLabel?.text = title
         self.section = section
         self.delegate = delegate
     }
     
-    override   func layoutSubviews() {
-        self.layoutSubviews()
+    override func layoutSubviews() {
+        super.layoutSubviews()
         self.textLabel?.textColor = UIColor.white
-        self.contentView.backgroundColor = UIColor.gray
-        
+        self.contentView.backgroundColor = UIColor.lightGray
     }
-
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
 }
---
+++ TeamWorkProject/ProjectViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ProjectViewController.swift
+//  TeamWorkProject
+//
+//  Created by Kaisa Tuononen on 8/20/17.
+//  Copyright © 2017 myorg. All rights reserved.
+//
+
+import UIKit
+
+class ProjectViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ExpandableHeaderViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sections = [
+        Section(genre: "🦁 Animation",
+                movies: ["The Lion King", "The Incredibles"],
+                expanded: false),
+        Section(genre: "💥 Superhero",
+                movies: ["Guardians of the Galaxy", "The Flash", "The Avengers", "The Dark Knight"],
+                expanded: false),
+        Section(genre: "👻 Horror",
+                movies: ["The Walking Dead", "Insidious", "Conjuring"],
+                expanded: false)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if (sections[indexPath.section].expanded) {
+            return 44
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = ExpandableHeaderView()
+        header.customInit(title: sections[section].genre, section: section, delegate: self)
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "labelCell")!
+        cell.textLabel?.text = sections[indexPath.section].movies[indexPath.row]
+        return cell
+    }
+    
+    func toggleSection(header: ExpandableHeaderView, section: Int) {
+        sections[section].expanded = !sections[section].expanded
+        
+        
+        tableView.beginUpdates()
+        for i in 0 ..< sections[section].movies.count {
+            tableView.reloadRows(at: [IndexPath(row: i, section: section)], with: .automatic)
+        }
+        tableView.endUpdates()
+    }
+    
+
+    
+
+
+}
